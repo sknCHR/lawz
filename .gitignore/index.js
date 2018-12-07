ee39d6b71337e720061d71475386284aba5fbabb
sknCHR <45670284+sknCHR@users.noreply.github.com>
2018-12-07 21:07:17 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,37 @@
 const Discord = require("discord.js");
 
+var PREFIX = "&";
 var bot = new Discord.Client();
 
 bot.on("ready", function(){
     bot.user.setGame("Teste");
     console.log("Bot is ready");
+});
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if (&message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIXlenght).split(" ");
+    switch(args[0].toLowerCase()) {
+        case "invite"
+        message.channel.send("", {
+            embed: {
+                color: 0xFF0000,
+                author: message.author.name,
+                title: '',
+                fields [{
+                    name : "Link"
+                    value:""
+                    inline: false
+                }],
+                footer: {
+                    footer: "test",
+                },
+            }
+        });
+        break; 
+    }
 });
 
 bot.login("NTAxODU1NzczNDI5NTk2MTYw.DuxTmw.RFkUMByv3RCZllwhDjb-6XQ-1oI")
